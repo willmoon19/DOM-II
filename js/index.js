f2a30367692969edd184fd6ac644c6e900106d98
William Moon <william-moon@lambdastudents.com>
2021-04-06 13:49:27 -0700

    made a keydown

--- js/index.js
+++ js/index.js
@@ -13,7 +13,19 @@ aboutButton.addEventListener("click", (event) => {
    event.target.style.color = "purple";
 })
 
-fun.addEventListener("mouseover", (event) => {
+fun.addEventListener("mouseenter", (event) => {
     event.target.style.color = "teal";
 })
 
+fun.addEventListener("mouseleave", (event) => {
+    event.target.style.color = "black";
+})
+
+const funClass = document.querySelector(".logo-heading")
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "1"){
+        funClass.style["font-size"] = "4rem";
+        console.log("hello");
+    }
+})
