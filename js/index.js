7df313c6bafa99d301be0c33ae5a26c3afd788ad
William Moon <william-moon@lambdastudents.com>
2021-04-06 13:05:57 -0700

    added click and mouseover for color change

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+//on click nav-link honk
+//button onclick change color
+// keydown move fun bus?
+//
+
+const fun = document.querySelector("h1")
+const navButtons = document.querySelectorAll("nav a")
+const aboutButton = navButtons[1];
+
+aboutButton.addEventListener("click", (event) => {
+   event.target.style.color = "purple";
+})
+
+fun.addEventListener("mouseover", (event) => {
+    event.target.style.color = "teal";
+})
+
